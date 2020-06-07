1cfa9b67fc794da79ac13a729efab78c1ff4f1f0
Tom Meyers <tom@odex.be>
2020-06-07 16:38:09 +0200

    Made the helper scripts fit in a more appropriate location

--- systemd/import-keys.sh
+++ systemd/import-keys.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # required keys to build systemd
 gpg --keyserver pool.sks-keyservers.net --recv-keys 5C251B5FC54EB2F80F407AAAC54CA336CFEB557E 63CDA1E5D3FC22B998D20DD6327F26951A015CC4
